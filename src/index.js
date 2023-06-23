8fe3894a129a0e23b984a9c71895b4c9b7478dda
BurnBean <cjh2294@gmail.com>
2023-06-23 16:29:25 +0900

    버그 수정, css 수정

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import App from './App';
 import './styles/style.scss';
 import '@sakun/system.css/dist/system.css';
 
-// axios.defaults.baseURL = "http://192.168.0.50:1788"; // 태욱님컴
-axios.defaults.baseURL = "http://192.168.0.81:1788"; // 태경님컴
+axios.defaults.baseURL = "http://192.168.0.50:1788"; // 태욱님컴
+// axios.defaults.baseURL = "http://192.168.0.81:1788"; // 태경님컴
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
